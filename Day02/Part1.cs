4b388593e50c0e83882be487cac9c4e55a33f78f
Michele Appiani <michele.appiani@gmail.com>
2023-12-03 10:27:58 +0800

    stub out day 02

---
+++ Day02/Part1.cs
@@ -0,0 +1,23 @@
+﻿namespace Day02;
+
+public class Part1
+{
+    public int Solve()
+    {
+        string? input;
+        int solution = 0;
+
+        using var sr = new StreamReader("input.txt");
+        while (!sr.EndOfStream)
+        {
+            input = sr.ReadLine();
+
+            if (input is null)
+            {
+                break;
+            }
+        }
+
+        return solution;
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,6 @@
+﻿using Day02;
+
+var part1 = new Part1();
+var part1Solution = part1.Solve();
+Console.WriteLine($"Part 1: {part1Solution}");
+

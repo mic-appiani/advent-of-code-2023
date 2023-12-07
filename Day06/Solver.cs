993ef338e08ebf0d0cf452213a1aaa22f2a16c13
Michele Appiani <michele.appiani@gmail.com>
2023-12-07 20:55:33 +0800

    create day 6 stub

---
+++ Day06/Solver.cs
@@ -0,0 +1,22 @@
+﻿public class Solver
+{
+    private int _solution = 0;
+
+    public int Solve(int part)
+    {
+        string? input;
+
+        using var sr = new StreamReader("input.txt");
+        while (!sr.EndOfStream)
+        {
+            input = sr.ReadLine();
+
+            if (input is null)
+            {
+                throw new Exception("input should not be null");
+            }
+        }
+
+        return _solution;
+    }
+}

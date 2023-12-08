83b2e137b00d552290fec64c907bf5f1b02e5a7d
Michele Appiani <michele.appiani@gmail.com>
2023-12-08 14:05:15 +0800

    add stub for day 7

---
+++ Day07/Solver.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+public class Solver
+{
+    private int _solution = 0;
+
+    public int Solve(int part)
+    {
+        using var sr = new StreamReader("input.txt");
+        string? input;
+        while (!sr.EndOfStream)
+        {
+            input = sr.ReadLine();
+        }
+
+        return _solution;
+    }
+}

fb8489983dd5bf662e886e956e6a56518dfa0852
Michele Appiani <michele.appiani@gmail.com>
2023-12-10 18:49:57 +0800

    stub day 08

---
+++ Day08/Solver.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Day08;
+
+public class Solver
+{
+    private int _solution;
+
+    public int Solve(int part)
+    {
+        using var sr = new StreamReader("input.txt");
+        while (!sr.EndOfStream)
+        {
+            var input = sr.ReadLine();
+        }
+
+        return _solution;
+    }
+}

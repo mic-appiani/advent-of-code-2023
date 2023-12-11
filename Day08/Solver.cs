bb0471051a6cb384d7a0fa78239b9f0e38e67ac5
Michele Appiani <michele.appiani@gmail.com>
2023-12-11 10:30:40 +0800

    day 08 part 2

--- Day08/Solver.cs
+++ Day08/Solver.cs
@@ -5,11 +5,11 @@ namespace Day08;
 
 public class Solver
 {
-    private int _solution;
+    private long _solution;
     private Dictionary<string, Node> _nodes = new Dictionary<string, Node>();
     private string? _instructions;
 
-    public int Solve(int part)
+    public long Solve(int part)
     {
         using var sr = new StreamReader("input.txt");
         _instructions = sr.ReadLine();
@@ -24,7 +24,8 @@ public class Solver
 
             var node = new Node
             {
-                Id = matches[0].Value,
+                IdPrefix = matches[0].Value.Substring(0, 2),
+                IdSuffix = matches[0].Value.Substring(2, 1),
                 Left = matches[1].Value,
                 Right = matches[2].Value,
             };
@@ -35,6 +36,75 @@ public class Solver
             }
         }
 
+        if (part == 1)
+        {
+            SolvePart1();
+        }
+        else if (part == 2)
+        {
+            SolvePart2();
+        }
+
+        return _solution;
+    }
+
+    private void SolvePart2()
+    {
+        // take all the nodes with suffix "A"
+        // iterate the instructions like on part 1, but only stop when the current nodes have suffix Z
+        var startNodes = _nodes
+            .Where(x => x.Value.IdSuffix == "A")
+            .Select(x => x.Value)
+            .ToArray();
+
+        var steps = new long[startNodes.Length];
+        // for each node find the steps required to reach z
+        for (int i = 0; i < startNodes.Length; i++)
+        {
+            var node = startNodes[i];
+            var idx = 0;
+
+            while (node.IdSuffix != "Z")
+            {
+                var direction = _instructions[idx];
+
+                var nextKey = direction == 'L'
+                    ? node.Left
+                    : node.Right;
+
+                node = _nodes[nextKey];
+                steps[i]++;
+
+                idx++;
+
+                if (idx >= _instructions.Length)
+                {
+                    idx = 0;
+                }
+            }
+        }
+
+        _solution = steps.Aggregate((a, b) => Lcm(a, b));
+    }
+    
+    static long Gcd(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    static long Lcm(long a, long b)
+    {
+        return (a * b) / Gcd(a, b);
+    }
+
+    private void SolvePart1()
+    {
         var currentNode = _nodes["AAA"];
         while (!currentNode.Id.Equals("ZZZ"))
         {
@@ -53,14 +123,19 @@ public class Solver
                 }
             }
         }
-
-        return _solution;
     }
 }
 
 public record Node
 {
-    public string Id { get; set; }
+    public string IdPrefix { get; set; }
+    public string IdSuffix { get; set; }
+
+    public string Id
+    {
+        get => IdPrefix + IdSuffix;
+    }
+
     public string Left { get; set; }
     public string Right { get; set; }
 }

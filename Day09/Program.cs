8e7648b01b9009e11d0f7a7dcd1fdbf4d7970c35
Michele Appiani <michele.appiani@gmail.com>
2023-12-12 20:05:52 +0800

    day 9 part 1

---
+++ Day09/Program.cs
@@ -0,0 +1,7 @@
+﻿var part1 = new Solver();
+var part1Solution = part1.Solve(1);
+Console.WriteLine($"Part 1: {part1Solution}");
+
+var part2 = new Solver();
+var part2Solution = part2.Solve(2);
+Console.WriteLine($"Part 2: {part2Solution}");
---
+++ Day09/Solver.cs
@@ -0,0 +1,54 @@
+﻿public class Solver
+{
+    private int _solution;
+
+    public int Solve(int part)
+    {
+        using var sr = new StreamReader("input.txt");
+
+        while (!sr.EndOfStream)
+        {
+            var input = sr.ReadLine();
+
+            var lastNumsInSequences = new List<int>();
+            var sequence = input!.Split(' ')
+                .Select(x => int.Parse(x)).ToList();
+
+            var allZeros = false;
+            while (!allZeros)
+            {
+                lastNumsInSequences.Add(sequence.Last());
+
+                var newSequence = new List<int>();
+
+                var zeros = 0;
+                for (int i = 1; i < sequence.Count; i++)
+                {
+                    var diff = sequence[i] - sequence[i - 1];
+
+                    if (diff == 0)
+                    {
+                        zeros++;
+                    }
+
+                    newSequence.Add(diff);
+                }
+
+                if (zeros == newSequence.Count)
+                {
+                    allZeros = true;
+                }
+
+                sequence = newSequence;
+            }
+
+
+            _solution += lastNumsInSequences.Sum();
+            // for each sequence, create a new sequence that is one smaller than the original sequence
+            // repeat until the sequence is all zeros
+            // sum the last element in each sequence including the original, this is the prediction
+        }
+
+        return _solution;
+    }
+}

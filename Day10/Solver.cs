0349cc85ce4de8e3808a4e57a3cb494ccc2f3530
Michele Appiani <michele.appiani@gmail.com>
2023-12-13 12:11:25 +0800

    way too much code to determine intial pipe and travel directions

--- Day10/Solver.cs
+++ Day10/Solver.cs
@@ -34,12 +34,50 @@ public class Solver
         }
 
         PrintMap();
+        Pipe pipe = FindEntryPipe(_startRow, _startCol);
+        Console.WriteLine();
+        // pick one direction and follow it
         // start from S and detect connected cells
         // assumption: is a continuous loop
         // continue until S is hit
         // divide steps by 2 (round up to next int)
-        
+
         return _solution;
+    }
+
+    private Pipe FindEntryPipe(int startRow, int startCol)
+    {
+        // for now let's ignore boundaries because i'm feeling lucky
+        // check connections up
+        var pipe = new Pipe(startRow - 1, startCol, _map);
+
+        if (pipe.CanConnectTo(startRow, startCol))
+        {
+            return pipe;
+        }
+
+        pipe = new Pipe(startRow + 1, startCol, _map);
+
+        if (pipe.CanConnectTo(startRow, startCol))
+        {
+            return pipe;
+        }
+
+        pipe = new Pipe(startRow, startCol - 1, _map);
+
+        if (pipe.CanConnectTo(startRow, startCol))
+        {
+            return pipe;
+        }
+
+        pipe = new Pipe(startRow, startCol + 1, _map);
+
+        if (pipe.CanConnectTo(startRow, startCol))
+        {
+            return pipe;
+        }
+
+        throw new Exception("The location cannot connect to any surrounding pipe");
     }
 
     private void PrintMap()
@@ -55,4 +93,150 @@ public class Solver
             Console.WriteLine(line);
         }
     }
+}
+
+public class Pipe
+{
+    public Pipe(int row, int col, List<List<char>> map)
+    {
+        Row = row;
+        Col = col;
+        Symbol = map[row][col];
+    }
+
+    public char Symbol { get; set; }
+    public int Row { get; set; }
+    public int Col { get; set; }
+
+    public bool CanConnectTo(int row, int col)
+    {
+        var direction = GetDirectionRelativeTo(row, col);
+        return Symbol switch
+        {
+            '|' => direction == Direction.South || direction == Direction.North,
+            '-' => direction == Direction.East || direction == Direction.West,
+            'L' => direction == Direction.East || direction == Direction.North,
+            'J' => direction == Direction.West || direction == Direction.North,
+            '7' => direction == Direction.West || direction == Direction.South,
+            'F' => direction == Direction.East || direction == Direction.South,
+            _ => false,
+        };
+    }
+
+    public int[] GetExit(int entryRow, int entryCol)
+    {
+        Direction entranceDirection = GetDirectionRelativeTo(entryRow, entryCol);
+
+        if (Symbol == '|')
+        {
+            if (entranceDirection is Direction.East or Direction.West)
+            {
+                throw new ArgumentException("Illegal arguments");
+            }
+
+            int exitRow = entranceDirection == Direction.South ? Row - 1 : Row + 1;
+            return new[] { exitRow, entryCol };
+        }
+
+        if (Symbol == '-')
+        {
+            if (entranceDirection is Direction.North or Direction.South)
+            {
+                throw new ArgumentException("Illegal arguments");
+            }
+
+            int exitCol = entranceDirection == Direction.East ? Col - 1 : Col + 1;
+            return new[] { entryRow, exitCol };
+        }
+
+        if (Symbol == 'L')
+        {
+            if (entranceDirection is Direction.West or Direction.South)
+            {
+                throw new ArgumentException("Illegal arguments");
+            }
+
+            var exitCol = entranceDirection == Direction.North ? Col + 1 : Col;
+            var exitRow = entranceDirection == Direction.North ? Row : Row - 1;
+            return new[] { exitRow, exitCol };
+        }
+
+        if (Symbol == 'J')
+        {
+            if (entranceDirection is Direction.East or Direction.South)
+            {
+                throw new ArgumentException("Illegal arguments");
+            }
+
+            var exitCol = entranceDirection == Direction.North ? Col - 1 : Col;
+            var exitRow = entranceDirection == Direction.North ? Row : Row - 1;
+            return new[] { exitRow, exitCol };
+        }
+
+        if (Symbol == '7')
+        {
+            if (entranceDirection is Direction.East or Direction.North)
+            {
+                throw new ArgumentException("Illegal arguments");
+            }
+
+            var exitCol = entranceDirection == Direction.South ? Col - 1 : Col;
+            var exitRow = entranceDirection == Direction.South ? Row : Row + 1;
+            return new[] { exitRow, exitCol };
+        }
+
+        if (Symbol == 'F')
+        {
+            if (entranceDirection is Direction.West or Direction.North)
+            {
+                throw new ArgumentException("Illegal arguments");
+            }
+
+            var exitCol = entranceDirection == Direction.South ? Col + 1 : Col;
+            var exitRow = entranceDirection == Direction.South ? Row : Row + 1;
+            return new[] { exitRow, exitCol };
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(Symbol));
+    }
+
+    /// <summary>
+    /// Does not support diagonals
+    /// </summary>
+    /// <param name="entryRow"></param>
+    /// <param name="entryCol"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    private Direction GetDirectionRelativeTo(int entryRow, int entryCol)
+    {
+        if (entryRow == Row - 1)
+        {
+            return Direction.North;
+        }
+
+        if (entryRow == Row + 1)
+        {
+            return Direction.South;
+        }
+
+        if (entryCol == Row - 1)
+        {
+            return Direction.West;
+        }
+
+        if (entryCol == Row + 1)
+        {
+            return Direction.East;
+        }
+
+        throw new ArgumentException("Invalid entry direction");
+    }
+}
+
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West
 }

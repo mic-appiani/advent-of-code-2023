6cd997c2e1df91c3d63001020eb192f194033800
Michele Appiani <michele.appiani@gmail.com>
2023-12-14 10:56:04 +0800

    create stub for day11

---
+++ Day11/Solver.cs
@@ -0,0 +1,18 @@
+﻿namespace Day11;
+
+public class Solver
+{
+    private int _solution;
+
+    public int Solve(int part)
+    {
+        using var sr = new StreamReader("input.txt");
+
+        while (!sr.EndOfStream)
+        {
+            var input = sr.ReadLine();
+        }
+
+        return _solution;
+    }
+}
